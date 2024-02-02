c97e5e2b4318923bc448ce77f8ebec0a5a5e17bc
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-02 23:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function isPalindrome(num) {
+        const numStr = num.toString();
+        const reversedNumStr = numStr.split('').reverse().join('');
+        return numStr === reversedNumStr;
+    }
+
+    // Find the next palindrome greater than the input number
+    let nextNumber = inputNumber + 1;
+    while (!isPalindrome(nextNumber)) {
+        nextNumber++;
+    }
+
+    return nextNumber;
 }
 
 const input = prompt("Enter a palindrome number");
